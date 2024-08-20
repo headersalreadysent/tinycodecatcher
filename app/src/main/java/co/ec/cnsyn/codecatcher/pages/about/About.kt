c98970332d77d88e13903b0db097c252a4185547
headersalreadysent <canosayan@canosayan.com>
2024-08-20 19:33:15 +0300

    add about page to app

---
+++ app/src/main/java/co/ec/cnsyn/codecatcher/pages/about/About.kt
@@ -0,0 +1,102 @@
+package co.ec.cnsyn.codecatcher.pages.about
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.ec.cnsyn.codecatcher.R
+import co.ec.cnsyn.codecatcher.composables.AutoText
+import co.ec.cnsyn.codecatcher.composables.SkewSquare
+import co.ec.cnsyn.codecatcher.composables.SkewSquareCut
+import co.ec.cnsyn.codecatcher.ui.theme.CodeCatcherTheme
+
+@Composable
+fun About() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 160.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        SkewSquare(
+            cut = SkewSquareCut.BottomEnd,
+            fill = MaterialTheme.colorScheme.primaryContainer,
+            modifier = Modifier
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .statusBarsPadding()
+                    .padding(bottom = 30.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "",
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                    modifier = Modifier.fillMaxWidth(.6F)
+                )
+
+                AutoText(
+                    text = stringResource(R.string.app_name),
+                    fontSize = 15..25,
+                    key = "about-app-name",
+                    style = MaterialTheme.typography.bodyLarge
+                        .copy(
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                )
+                Text(text = "Developed with Jetpack Compose")
+            }
+        }
+        val context = LocalContext.current
+        Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = stringResource(R.string.about_help_to_translate))
+            var title = stringResource(R.string.about_translate_mail_title)
+            OutlinedButton(onClick = {
+                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:")
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf("code-catcher-translate@proxiedmail.com"))
+                    putExtra(Intent.EXTRA_SUBJECT, title)
+                    putExtra(Intent.EXTRA_TEXT, "")
+                }
+                context.startActivity(intent)
+
+            }) {
+                Text(text = stringResource(R.string.about_translate_contact))
+            }
+        }
+
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AboutPreview() {
+    CodeCatcherTheme {
+        About()
+    }
+}
