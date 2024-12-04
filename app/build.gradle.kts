877242c5746ad3a0e00a5531d4c15712b18b93e3
headersalreadysent <canosayan@canosayan.com>
2024-12-04 15:48:57 +0300

    add turkish metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "co.ec.cnsyn.codecatcher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 115
-        versionName = "1.0.115"
+        versionCode = 116
+        versionName = "1.0.116"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
