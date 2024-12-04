c8f2b1878aa224c9dcedd406c6dfea99d369edeb
headersalreadysent <canosayan@canosayan.com>
2024-12-04 16:53:57 +0300

    fix release name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "co.ec.cnsyn.codecatcher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 119
-        versionName = "1.0.119"
+        versionCode = 120
+        versionName = "1.0.120"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
