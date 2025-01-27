cb1c9017f6a0d23322f779b250ca813742397c6b
headersalreadysent <canosayan@canosayan.com>
2025-01-27 16:31:28 +0300

    fix store text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "co.ec.cnsyn.codecatcher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 130
-        versionName = "1.0.130"
+        versionCode = 131
+        versionName = "1.0.131"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
